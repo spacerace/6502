403a4afda8ac8423c578ded7bb1e298b69838569
spacerace <nils.stec@gmail.com>
2013-03-05 23:15:17 +0200

    working

--- src/include/ncui_new.h
+++ src/include/ncui_new.h
@@ -1,10 +1,12 @@
 #ifndef __NCUI_NEW_H__
 #define __NCUI_NEW_H__
 
+#define SH_COMMAND	"/bin/bash"
 
 extern int ncui();
 
 /* only for interal use */
+static int get_system_shell(char *cmd_str);
 static int init_ncui();
 static int deinit_ncui();
 static int window_create_main();
--- src/ncui_new.c
+++ src/ncui_new.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "6502.h"
 #include "ncui_new.h"
@@ -16,15 +18,55 @@ int ncui() {
 	init_ncui();
 	window_create_main();
 	window_update_main();
+
+	if(0) get_system_shell(NULL);
+
+	getch();
+	
 	deinit_ncui();
 	return 0;
 }
 
+static int get_system_shell(char *cmd_str) {
+	int sh_ret = -1;
+
+	def_prog_mode();	// save tty modes
+	endwin();		// temporarily leave ncurses mode
+	
+	if(cmd_str == NULL) {
+		_logf("ncui-new: get_system_shell() running '%s'", SH_COMMAND);
+		sh_ret = system(SH_COMMAND);
+	} else {
+		_logf("ncui-new: get_system_shell() running '%s'", cmd_str);
+		sh_ret = system(cmd_str);
+	}
+
+	/* TODO */
+	/* add a "wait for keypress" func here */
+
+	_logf("ncui-new: get_system_shell() got '%d' from shell", sh_ret);
+
+	reset_prog_mode();	// go back to ncurses mode
+	refresh();
+
+	return sh_ret;
+}
+
 static int window_create_main() {
+	ui.win_main = newwin(ui.screen_y, ui.screen_x, 0, 0);	// lines, cols, beginy, beginx
+	bkgd(COLOR_PAIR(1));
+	wbkgd(ui.win_main, COLOR_PAIR(1));
+
+	wattrset(ui.win_main, A_BOLD|COLOR_PAIR(4));	
+	mvwprintw(ui.win_main, 0, 0, "File Debugger Settings Windows Help");
+	refresh();
+	wrefresh(ui.win_main);
+
 	return 0;
 }
 
 static int window_update_main() {
+	wrefresh(ui.win_main);
 	return 0;
 }
 
@@ -45,12 +87,18 @@ static int init_ncui() {
 		exit(-2);
 	}
 
+	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
+	init_pair(3, COLOR_BLACK, COLOR_RED);
+	init_pair(4, COLOR_YELLOW, COLOR_RED);
+
 	_logf("ncui-new: ncurses init ok!");
 
 	getmaxyx(stdscr, ui.screen_y, ui.screen_x);
 	
 	_logf("ncui-new: screen size is %d*%d chars", ui.screen_y, ui.screen_x);
 
+	
 
 	return 0;
 }
